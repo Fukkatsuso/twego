0055a0c8793e58985bc3ae114726cc0f0c9a95d1
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-03-31 21:48:34 +0900

    add-rules request

--- main.go
+++ main.go
@@ -1,7 +1,90 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+type SearchRule struct {
+	Value string `json:"value"`
+	Tag   string `json:"tag,omitempty"`
+}
+
+func AddSearchRules(bearerToken string, rules []SearchRule) error {
+	const endpoint = "https://api.twitter.com/2/tweets/search/stream/rules"
+
+	js, err := json.Marshal(struct {
+		Add []SearchRule `json:"add"`
+	}{
+		Add: rules,
+	})
+	if err != nil {
+		return err
+	}
+
+	reqBody := bytes.NewBuffer(js)
+
+	req, err := http.NewRequest(http.MethodPost, endpoint, reqBody)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", bearerToken))
+	req.Header.Add("Content-type", "application/json")
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var res struct {
+		Data []struct {
+			Value string `json:"value"`
+			Tag   string `json:"tag,omitempty"`
+			ID    string `json:"id"`
+		} `json:"data"`
+		Meta struct {
+			Sent    time.Time `json:"sent"`
+			Summary struct {
+				Created    int `json:"created"`
+				NotCreated int `json:"not_created"`
+				Valid      int `json:"valid"`
+				Invalid    int `json:"invalid"`
+			} `json:"summary"`
+		} `json:"meta"`
+	}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return err
+	}
+	fmt.Printf("res: %+v\n", res)
+
+	return nil
+}
 
 func main() {
-	fmt.Println("hello world!")
+	bearerToken := os.Getenv("TWITTER_BEARER_TOKEN")
+
+	rules := []SearchRule{
+		{
+			Value: "golang -is:retweet",
+			Tag:   "golang",
+		},
+	}
+
+	err := AddSearchRules(bearerToken, rules)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
