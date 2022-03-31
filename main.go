44563c0f2a62453cffc0e939f5a3a4f7adcae6d2
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-04-01 00:18:55 +0900

    get-rules request

--- main.go
+++ main.go
@@ -127,6 +127,47 @@ func DeleteSearchRules(bearerToken string, ids []string) error {
 	return nil
 }
 
+func GetSearchRules(bearerToken string) error {
+	const endpoint = "https://api.twitter.com/2/tweets/search/stream/rules"
+
+	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", bearerToken))
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var res struct {
+		Data []struct {
+			ID    string `json:"id"`
+			Value string `json:"value"`
+			Tag   string `json:"tag,omitempty"`
+		} `json:"data"`
+		Meta struct {
+			Sent        time.Time `json:"sent"`
+			ResultCount int       `json:"result_count"`
+		} `json:"meta"`
+	}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return err
+	}
+	fmt.Printf("res: %+v\n", res)
+
+	return nil
+}
+
 func main() {
 	bearerToken := os.Getenv("TWITTER_BEARER_TOKEN")
 
@@ -142,8 +183,13 @@ func main() {
 	// 	fmt.Println(err)
 	// }
 
-	ids := []string{"1509544617284280327"}
-	err := DeleteSearchRules(bearerToken, ids)
+	// ids := []string{"1509544617284280327"}
+	// err := DeleteSearchRules(bearerToken, ids)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	err := GetSearchRules(bearerToken)
 	if err != nil {
 		fmt.Println(err)
 	}
