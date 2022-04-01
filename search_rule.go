b6ddd52f383ce121d3fbc90f56d2fbb1666c3b07
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-04-01 19:29:31 +0900

    fix: file name

--- search_rule.go
+++ search_rule.go
@@ -1,58 +1,168 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
 
-type Tweet struct {
-	Data struct {
-		ID   string `json:"id"`
-		Text string `json:"text"`
-	} `json:"data"`
-	MatchingRules []struct {
-		ID  string `json:"id"`
-		Tag string `json:"tag"`
-	} `json:"matching_rules"`
+type SearchRule struct {
+	Value string `json:"value"`
+	Tag   string `json:"tag,omitempty"`
 }
 
-func GetTweetStream(bearerToken string) <-chan Tweet {
-	stream := make(chan Tweet)
+func AddSearchRules(bearerToken string, rules []SearchRule) error {
+	const endpoint = "https://api.twitter.com/2/tweets/search/stream/rules"
 
-	go func() {
-		defer close(stream)
+	js, err := json.Marshal(struct {
+		Add []SearchRule `json:"add"`
+	}{
+		Add: rules,
+	})
+	if err != nil {
+		return err
+	}
 
-		const endpoint = "https://api.twitter.com/2/tweets/search/stream"
+	reqBody := bytes.NewBuffer(js)
 
-		req, err := http.NewRequest(http.MethodGet, endpoint, nil)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+	req, err := http.NewRequest(http.MethodPost, endpoint, reqBody)
+	if err != nil {
+		return err
+	}
 
-		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", bearerToken))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", bearerToken))
+	req.Header.Add("Content-type", "application/json")
 
-		client := new(http.Client)
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer resp.Body.Close()
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-		decoder := json.NewDecoder(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
 
-		for {
-			var tweet Tweet
-			if err := decoder.Decode(&tweet); err != nil {
-				fmt.Println(err)
-				return
-			}
+	var res struct {
+		Data []struct {
+			Value string `json:"value"`
+			Tag   string `json:"tag,omitempty"`
+			ID    string `json:"id"`
+		} `json:"data"`
+		Meta struct {
+			Sent    time.Time `json:"sent"`
+			Summary struct {
+				Created    int `json:"created"`
+				NotCreated int `json:"not_created"`
+				Valid      int `json:"valid"`
+				Invalid    int `json:"invalid"`
+			} `json:"summary"`
+		} `json:"meta"`
+	}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return err
+	}
+	fmt.Printf("res: %+v\n", res)
 
-			stream <- tweet
-		}
-	}()
+	return nil
+}
 
-	return stream
+func DeleteSearchRules(bearerToken string, ids []string) error {
+	const endpoint = "https://api.twitter.com/2/tweets/search/stream/rules"
+
+	var delete struct {
+		Delete struct {
+			Ids []string `json:"ids"`
+		} `json:"delete"`
+	}
+	delete.Delete.Ids = ids
+
+	js, err := json.Marshal(delete)
+	if err != nil {
+		return err
+	}
+
+	reqBody := bytes.NewBuffer(js)
+
+	req, err := http.NewRequest(http.MethodPost, endpoint, reqBody)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", bearerToken))
+	req.Header.Add("Content-type", "application/json")
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var res struct {
+		Meta struct {
+			Sent    time.Time `json:"sent"`
+			Summary struct {
+				Deleted    int `json:"deleted"`
+				NotDeleted int `json:"not_deleted"`
+			} `json:"summary"`
+		} `json:"meta"`
+	}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return err
+	}
+	fmt.Printf("res: %+v\n", res)
+
+	return nil
+}
+
+func GetSearchRules(bearerToken string) error {
+	const endpoint = "https://api.twitter.com/2/tweets/search/stream/rules"
+
+	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", bearerToken))
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var res struct {
+		Data []struct {
+			ID    string `json:"id"`
+			Value string `json:"value"`
+			Tag   string `json:"tag,omitempty"`
+		} `json:"data"`
+		Meta struct {
+			Sent        time.Time `json:"sent"`
+			ResultCount int       `json:"result_count"`
+		} `json:"meta"`
+	}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return err
+	}
+	fmt.Printf("res: %+v\n", res)
+
+	return nil
 }
