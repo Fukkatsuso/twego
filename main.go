8301c9986f072ada9997043143318857ce1536bc
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-04-01 16:54:27 +0900

    get-tweet-stream request

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
+	"text/tabwriter"
 	"time"
 )
 
@@ -168,6 +170,57 @@ func GetSearchRules(bearerToken string) error {
 	return nil
 }
 
+type Tweet struct {
+	Data struct {
+		ID   string `json:"id"`
+		Text string `json:"text"`
+	} `json:"data"`
+	MatchingRules []struct {
+		ID  string `json:"id"`
+		Tag string `json:"tag"`
+	} `json:"matching_rules"`
+}
+
+func GetTweetStream(bearerToken string) <-chan Tweet {
+	stream := make(chan Tweet)
+
+	go func() {
+		defer close(stream)
+
+		const endpoint = "https://api.twitter.com/2/tweets/search/stream"
+
+		req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", bearerToken))
+
+		client := new(http.Client)
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+
+		decoder := json.NewDecoder(resp.Body)
+
+		for {
+			var tweet Tweet
+			if err := decoder.Decode(&tweet); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			stream <- tweet
+		}
+	}()
+
+	return stream
+}
+
 func main() {
 	bearerToken := os.Getenv("TWITTER_BEARER_TOKEN")
 
@@ -189,8 +242,32 @@ func main() {
 	// 	fmt.Println(err)
 	// }
 
-	err := GetSearchRules(bearerToken)
-	if err != nil {
-		fmt.Println(err)
+	// err := GetSearchRules(bearerToken)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 2, 0, ' ', 0)
+
+	stream := GetTweetStream(bearerToken)
+	for {
+		select {
+		case tweet, ok := <-stream:
+			if !ok {
+				fmt.Println("stream is closed")
+				return
+			}
+
+			now := time.Now().Format("2006/01/02 15:04:05")
+			texts := strings.Split(tweet.Data.Text, "\n")
+			for i, text := range texts {
+				if i == 0 {
+					fmt.Fprintln(w, now, "\t", text)
+				} else {
+					fmt.Fprintln(w, "\t", text)
+				}
+			}
+			w.Flush()
+		}
 	}
 }
