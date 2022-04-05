5fb50313662b2dd08b567a5cf600cca0b3c8663d
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-04-05 16:24:03 +0900

    add: unauth command

---
+++ cmd/unauth.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(unauthCmd)
+}
+
+var unauthCmd = &cobra.Command{
+	Use: "unauth",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return Unauth(defaultConfigFilepath())
+	},
+}
+
+func Unauth(configFilepath string) error {
+	config.BearerToken = ""
+	return writeConfig(config, configFilepath)
+}
